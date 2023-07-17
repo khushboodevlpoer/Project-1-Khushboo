748df36d8b518128a72cff75f9dac18e13550989
khushboodevlpoer <113016240+khushboodevlpoer@users.noreply.github.com>
2023-07-17 21:44:27 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,403 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.7,
+    price: 15,
+    img: 'images/biryani/pexels-ambur.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.1,
+    price: 18,
+    img: 'images/biryani/pexels-hyderabadi.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 3.3,
+    price: 10,
+    img: 'images/biryani/pexels-egg.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Chicken Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/pexels-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.4,
+    price: 14,
+    img: 'images/biryani/pexels-fish.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Hyd Mutton biryani',
+    category : 'biryani',
+    rating : 4.8,
+    price: 22,
+    img: 'images/biryani/pexels-mutton.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 2.3,
+    price: 10,
+    img: 'images/biryani/pexels-Kamrupi.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.9,
+    price: 22,
+    img: 'images/biryani/pexels-memoni.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 2.3,
+    price: 9,
+    img: 'images/biryani/pexels-mughlai.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Kashmiri Pulao',
+    category : 'chicken',
+    rating : 4.4,
+    price: 21,
+    img: 'images/chicken/chicken-curry.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Lemon Garlic',
+    category : 'chicken',
+    rating : 3.3,
+    price: 11,
+    img: 'images/chicken/chicken-do-pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Parmesan',
+    category : 'chicken',
+    rating : 4.5,
+    price: 19,
+    img: 'images/chicken/chicken-masala.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Alfredo',
+    category : 'chicken',
+    rating : 4.6,
+    price: 22,
+    img: 'images/chicken/chicken-roast.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Mustard Glazed',
+    category : 'chicken',
+    rating : 3.3,
+    price: 18,
+    img: 'images/chicken/handi-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Spicy Szechuan',
+    category : 'chicken',
+    rating : 3.9,
+    price: 20,
+    img: 'images/chicken/murgh-musallam.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Butter Paneer',
+    category : 'paneer',
+    rating : 3.3,
+    price: 11,
+    img: 'images/paneer/butter-paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Chilli Paneer',
+    category : 'paneer',
+    rating : 2.9,
+    price: 10,
+    img: 'images/paneer/chilli-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Kadai Paneer',
+    category : 'paneer',
+    rating : 3.9,
+    price: 16,
+    img: 'images/paneer/kadai-paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Matter Paneer',
+    category : 'paneer',
+    rating : 4.9,
+    price: 29,
+    img: 'images/paneer/mattar-paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 3.3,
+    price: 10,
+    img: 'images/paneer/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Paneer Bhurji',
+    category : 'paneer',
+    rating : 3.9,
+    price: 9,
+    img: 'images/paneer/paneer-bhurji.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Paneer Korma',
+    category : 'paneer',
+    rating : 4.4,
+    price: 15,
+    img: 'images/paneer/paneer-korma.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Aaloo Gobhi',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 9,
+    img: 'images/vegetable/aaloo-gobhi.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Brockly Potato',
+    category : 'vegetable',
+    rating : 4.5,
+    price: 8,
+    img: 'images/vegetable/brockly-potato.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Dal Fry',
+    category : 'vegetable',
+    rating : 4.6,
+    price: 10,
+    img: 'images/vegetable/dal-fry.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Gobhi Manchurian',
+    category : 'vegetable',
+    rating : 4.9,
+    price: 17,
+    img: 'images/vegetable/gobhi-manchurian.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Rajma',
+    category : 'vegetable',
+    rating : 3.3,
+    price: 11,
+    img: 'images/vegetable/rajma.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Garlic Mashroom',
+    category : 'vegetable',
+    rating : 4.5,
+    price: 10,
+    img: 'images/vegetable/spicy garlic mashroom.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Vegetable Biryani',
+    category : 'vegetable',
+    rating : 3.3,
+    price: 5,
+    img: 'images/vegetable/vegetable-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Aaloo Gobhi',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/pexels-ch1.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Cabbage Mommos',
+    category : 'chinese',
+    rating : 4.4,
+    price: 17,
+    img: 'images/chinese/pexels-ch2.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Sweet and Sour',
+    category : 'chinese',
+    rating : 4.9,
+    price: 15,
+    img: 'images/chinese/pexels-ch3.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Mongolian Beef',
+    category : 'chinese',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chinese/pexels-ch4.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.9,
+    price: 18,
+    img: 'images/chinese/pexels-ch5.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Chinese Burger',
+    category : 'chinese',
+    rating : 3.3,
+    price: 10,
+    img: 'images/chinese/chinese-burger.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Spicy Fry',
+    category : 'chinese',
+    rating : 4.6,
+    price: 34,
+    img: 'images/chinese/pexels-ch6.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Apam',
+    category : 'south indian',
+    rating : 4.8,
+    price: 38,
+    img: 'images/south indian/apam.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 3.9,
+    price: 29,
+    img: 'images/south indian/butter-masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Coconut Rice',
+    category : 'south indian',
+    rating : 4.4,
+    price: 32,
+    img: 'images/south indian/coconut-rice.webp',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Malaba Paratha',
+    category : 'south indian',
+    rating : 4.7,
+    price: 27,
+    img: 'images/south indian/malabar-paratha.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.1,
+    price: 19,
+    img: 'images/south indian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Peasarathu',
+    category : 'south indian',
+    rating : 4.3,
+    price: 30,
+    img: 'images/south indian/peasarathu.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Putu',
+    category : 'south indian',
+    rating : 3.9,
+    price: 25,
+    img: 'images/south indian/putu.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.9,
+    price: 35,
+    img: 'images/south indian/rava-uttapam.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
